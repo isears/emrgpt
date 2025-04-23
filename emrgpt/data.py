7670284966108dd5743d8b12e2b64f2826f37cc8
isears <isaac.j.sears@gmail.com>
2025-04-23 18:16:55 +0000

    move data processing code to its own file

--- emrgpt/data.py
+++ emrgpt/data.py
@@ -1,67 +1,193 @@
 from torch.utils.data import Dataset, DataLoader, Subset
+from psycopg2.extensions import AsIs
+import pandas as pd
+import atexit
+import psycopg2
+import psycopg2.extras
 import torch
-import lightning as L
-from emrgpt import BasicDM
 
 
-class ShakespeareDS(Dataset):
-    def __init__(self, path: str, block_size: int):
+class TimelineDS(Dataset):
+
+    def __init__(self, block_size: int):
         super().__init__()
-        torch.manual_seed(1337)
 
-        self.path = path
         self.block_size = block_size
 
-        with open(self.path, "r", encoding="utf-8") as f:
-            self.text = f.read()
+        # Temporary conn to get metadata (e.g. len)
+        # NOTE: assuming database info in environ
+        # i.e. PGHOST, PGUSER, PGPASSWORD, PGDATABASE
+        c = psycopg2.connect("")
+        cursor = c.cursor()
+        cursor.execute(
+            """
+            --sql
+            SELECT stay_id FROM mimiciv_derived.icustay_times 
+            WHERE (outtime_hr - intime_hr) > INTERVAL '%s hours';
+            """,
+            (block_size,),
+        )
+        res = cursor.fetchall()
+        self.stay_ids = [i[0] for i in res]
 
-        self.chars = sorted(list(set(self.text)))
-        self.vocab_size = len(self.chars)
+        # Also going to go ahead and calculate min / max for normalization
+        # NOTE: this should technically be done per train / val split
+        cursor.execute(
+            """
+            --sql
+            SELECT * FROM information_schema.columns
+            WHERE table_schema = 'mimiciv_local'
+            AND table_name = 'timebuckets';
+            """
+        )
 
-        self.encoding_map = {ch: i for i, ch in enumerate(self.chars)}
-        self.decoding_map = {i: ch for i, ch in enumerate(self.chars)}
+        res = cursor.fetchall()
+        self.features = [row[3] for row in res if row[3] not in ["stay_id", "tidx"]]
+        self.feature_stats = dict()
 
-    def encode(self, text: str) -> torch.Tensor:
-        encoded = [self.encoding_map[c] for c in text]
-        return torch.tensor(encoded, dtype=torch.long)
+        for f in self.features:
+            cursor.execute(
+                """
+                --sql
+                SELECT
+                    MIN(%s),
+                    MAX(%s),
+                    AVG(%s),
+                    STDDEV(%s)
+                FROM mimiciv_local.timebuckets;
+                """,
+                (AsIs(f),) * 4,
+            )
+            res = cursor.fetchall()
 
-    def decode(self, data: torch.Tensor) -> str:
-        decoded = [self.decoding_map[i] for i in data.tolist()]
-        return "".join(decoded)
+            self.feature_stats[f] = {
+                "min": res[0][0],
+                "max": res[0][1],
+                "avg": res[0][2],
+                "stddev": res[0][3],
+            }
+
+        c.close()
+
+        self.conn = None
+
+    def normalize(self, data: torch.Tensor) -> torch.Tensor:
+        assert data.ndim == 2
+        assert data.shape[1] == len(self.features)
+
+        max_t = torch.tensor(
+            [v["max"] for k, v in self.feature_stats.items()],
+            dtype=torch.float,
+            device=data.device,
+        )
+        min_t = torch.tensor(
+            [v["min"] for k, v in self.feature_stats.items()],
+            dtype=torch.float,
+            device=data.device,
+        )
+
+        return (data - min_t) / (max_t - min_t)
+
+    def denormalize(self, data: torch.Tensor) -> torch.Tensor:
+        max_t = torch.tensor(
+            [v["max"] for k, v in self.feature_stats.items()],
+            dtype=torch.float,
+            device=data.device,
+        )
+        min_t = torch.tensor(
+            [v["min"] for k, v in self.feature_stats.items()],
+            dtype=torch.float,
+            device=data.device,
+        )
+
+        return (data * (max_t - min_t)) + min_t
+
+    def standardize(self, data: torch.Tensor) -> torch.Tensor:
+        assert data.ndim == 2
+        assert data.shape[1] == len(self.features)
+
+        mu = torch.tensor(
+            [v["avg"] for k, v in self.feature_stats.items()],
+            dtype=torch.float,
+            device=data.device,
+        )
+        sigma = torch.tensor(
+            [v["stddev"] for k, v in self.feature_stats.items()],
+            dtype=torch.float,
+            device=data.device,
+        )
+
+        return (data - mu) / sigma
+
+    def destandardize(self, data: torch.Tensor) -> torch.Tensor:
+        assert data.ndim == 2
+        assert data.shape[1] == len(self.features)
+
+        mu = torch.tensor(
+            [v["avg"] for k, v in self.feature_stats.items()],
+            dtype=torch.float,
+            device=data.device,
+        )
+        sigma = torch.tensor(
+            [v["stddev"] for k, v in self.feature_stats.items()],
+            dtype=torch.float,
+            device=data.device,
+        )
+
+        return (data * sigma) + mu
+
+    def _lazy_get_conn(self):
+        self.conn = psycopg2.connect("")
+        atexit.register(self._teardown)
+
+    def _teardown(self):
+        print("Dataset teardown called")
+        self.conn.close()
 
     def __len__(self):
-        return len(self.text)
+        return len(self.stay_ids)
 
-    def __getitem__(self, index: int) -> tuple[torch.Tensor, torch.Tensor]:
-        x = self.text[index : index + self.block_size]
-        y = self.text[index + 1 : index + self.block_size + 1]
+    def __getitem__(self, index):
+        this_sid = self.stay_ids[index]
 
-        return self.encode(x), self.encode(y)
+        if self.conn is None:
+            self._lazy_get_conn()
 
-
-class ShakespeareDM(BasicDM):
-    def __init__(self, path: str, batch_size: int, block_size: int):
-        super().__init__(ShakespeareDS(path, block_size), batch_size)
-
-    def setup(self, stage: str):
-        n = int(0.9 * len(self.core_ds))
-        self.train_ds = Subset(self.core_ds, indices=range(0, n))
-        self.valid_ds = Subset(
-            self.core_ds, indices=range(n, len(self.core_ds) - self.core_ds.block_size)
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute(
+            """
+            --sql
+            SELECT * FROM mimiciv_local.timebuckets WHERE stay_id = %s ORDER BY tidx ASC;
+            """,
+            (this_sid,),
         )
 
-    def train_dataloader(self):
-        return DataLoader(
-            self.train_ds,
-            num_workers=self.cores_available,
-            batch_size=self.batch_size,
-            shuffle=True,
-        )
+        res = cursor.fetchall()
 
-    def val_dataloader(self):
-        return DataLoader(
-            self.valid_ds,
-            num_workers=self.cores_available,
-            batch_size=self.batch_size,
-            shuffle=False,
+        df = pd.DataFrame(res)
+        df.drop(columns=["tidx", "stay_id"], inplace=True)
+
+        if len(df) < self.block_size + 1:
+            pad_len = (self.block_size + 1) - len(df)
+            pad = pd.DataFrame(data={cname: [pd.NA] * pad_len for cname in df.columns})
+            df = pd.concat([df, pad])
+        elif len(df) == self.block_size + 1:
+            pass
+
+        else:
+            start_idx = torch.randint(0, (len(df) - (self.block_size + 1)), (1,)).item()
+            df = df[start_idx : start_idx + self.block_size + 1]
+
+        # TODO: is there any utility to also passing an x_nanmask?
+        X = df[0 : self.block_size]
+        y = df[1 : self.block_size + 1]
+        y_nanmask = torch.tensor(y.isna().values, dtype=torch.bool)
+
+        y = y.fillna(0.0)
+        X = X.fillna(0.0)
+
+        return (
+            self.normalize(torch.tensor(X.values, dtype=torch.float)),
+            self.normalize(torch.tensor(y.values, dtype=torch.float)),
+            y_nanmask,
         )
--- emrgpt/trainer.py
+++ emrgpt/trainer.py
@@ -1,15 +1,12 @@
 from torch.utils.data import Dataset, DataLoader, Subset, random_split
-import psycopg2
-import psycopg2.extras
 import torch
-import pandas as pd
-import atexit
 from emrgpt.model import TimelineBasedEmrGPT
 from torchinfo import summary
 import torch.nn.functional as F
 import warnings
 from tqdm import tqdm
-from psycopg2.extensions import AsIs
+
+from emrgpt.data import TimelineDS
 
 # stfu pandas
 warnings.simplefilter(action="ignore", category=FutureWarning)
@@ -25,190 +22,6 @@ N_EMBD = 32
 DROPOUT = 0.2
 DL_WORKERS = 6
 VAL_CHECK_INTERVAL = 100
-
-
-class TimelineDS(Dataset):
-
-    def __init__(self):
-        super().__init__()
-
-        # Temporary conn to get metadata (e.g. len)
-        # NOTE: assuming database info in environ
-        # i.e. PGHOST, PGUSER, PGPASSWORD, PGDATABASE
-        c = psycopg2.connect("")
-        cursor = c.cursor()
-        cursor.execute(
-            """
-            --sql
-            SELECT stay_id FROM mimiciv_derived.icustay_times 
-            WHERE (outtime_hr - intime_hr) > INTERVAL '%s hours';
-            """,
-            (BLOCK_SIZE,),
-        )
-        res = cursor.fetchall()
-        self.stay_ids = [i[0] for i in res]
-
-        # Also going to go ahead and calculate min / max for normalization
-        # NOTE: this should technically be done per train / val split
-        cursor.execute(
-            """
-            --sql
-            SELECT * FROM information_schema.columns
-            WHERE table_schema = 'mimiciv_local'
-            AND table_name = 'timebuckets';
-            """
-        )
-
-        res = cursor.fetchall()
-        self.features = [row[3] for row in res if row[3] not in ["stay_id", "tidx"]]
-        self.feature_stats = dict()
-
-        for f in self.features:
-            cursor.execute(
-                """
-                --sql
-                SELECT
-                    MIN(%s),
-                    MAX(%s),
-                    AVG(%s),
-                    STDDEV(%s)
-                FROM mimiciv_local.timebuckets;
-                """,
-                (AsIs(f),) * 4,
-            )
-            res = cursor.fetchall()
-
-            self.feature_stats[f] = {
-                "min": res[0][0],
-                "max": res[0][1],
-                "avg": res[0][2],
-                "stddev": res[0][3],
-            }
-
-        c.close()
-
-        self.conn = None
-
-    def normalize(self, data: torch.Tensor) -> torch.Tensor:
-        assert data.ndim == 2
-        assert data.shape[1] == len(self.features)
-
-        max_t = torch.tensor(
-            [v["max"] for k, v in self.feature_stats.items()],
-            dtype=torch.float,
-            device=data.device,
-        )
-        min_t = torch.tensor(
-            [v["min"] for k, v in self.feature_stats.items()],
-            dtype=torch.float,
-            device=data.device,
-        )
-
-        return (data - min_t) / (max_t - min_t)
-
-    def denormalize(self, data: torch.Tensor) -> torch.Tensor:
-        max_t = torch.tensor(
-            [v["max"] for k, v in self.feature_stats.items()],
-            dtype=torch.float,
-            device=data.device,
-        )
-        min_t = torch.tensor(
-            [v["min"] for k, v in self.feature_stats.items()],
-            dtype=torch.float,
-            device=data.device,
-        )
-
-        return (data * (max_t - min_t)) + min_t
-
-    def standardize(self, data: torch.Tensor) -> torch.Tensor:
-        assert data.ndim == 2
-        assert data.shape[1] == len(self.features)
-
-        mu = torch.tensor(
-            [v["avg"] for k, v in self.feature_stats.items()],
-            dtype=torch.float,
-            device=data.device,
-        )
-        sigma = torch.tensor(
-            [v["stddev"] for k, v in self.feature_stats.items()],
-            dtype=torch.float,
-            device=data.device,
-        )
-
-        return (data - mu) / sigma
-
-    def destandardize(self, data: torch.Tensor) -> torch.Tensor:
-        assert data.ndim == 2
-        assert data.shape[1] == len(self.features)
-
-        mu = torch.tensor(
-            [v["avg"] for k, v in self.feature_stats.items()],
-            dtype=torch.float,
-            device=data.device,
-        )
-        sigma = torch.tensor(
-            [v["stddev"] for k, v in self.feature_stats.items()],
-            dtype=torch.float,
-            device=data.device,
-        )
-
-        return (data * sigma) + mu
-
-    def _lazy_get_conn(self):
-        self.conn = psycopg2.connect("")
-        atexit.register(self._teardown)
-
-    def _teardown(self):
-        print("Dataset teardown called")
-        self.conn.close()
-
-    def __len__(self):
-        return len(self.stay_ids)
-
-    def __getitem__(self, index):
-        this_sid = self.stay_ids[index]
-
-        if self.conn is None:
-            self._lazy_get_conn()
-
-        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        cursor.execute(
-            """
-            --sql
-            SELECT * FROM mimiciv_local.timebuckets WHERE stay_id = %s ORDER BY tidx ASC;
-            """,
-            (this_sid,),
-        )
-
-        res = cursor.fetchall()
-
-        df = pd.DataFrame(res)
-        df.drop(columns=["tidx", "stay_id"], inplace=True)
-
-        if len(df) < BLOCK_SIZE + 1:
-            pad_len = (BLOCK_SIZE + 1) - len(df)
-            pad = pd.DataFrame(data={cname: [pd.NA] * pad_len for cname in df.columns})
-            df = pd.concat([df, pad])
-        elif len(df) == BLOCK_SIZE + 1:
-            pass
-
-        else:
-            start_idx = torch.randint(0, (len(df) - (BLOCK_SIZE + 1)), (1,)).item()
-            df = df[start_idx : start_idx + BLOCK_SIZE + 1]
-
-        # TODO: is there any utility to also passing an x_nanmask?
-        X = df[0:BLOCK_SIZE]
-        y = df[1 : BLOCK_SIZE + 1]
-        y_nanmask = torch.tensor(y.isna().values, dtype=torch.bool)
-
-        y = y.fillna(0.0)
-        X = X.fillna(0.0)
-
-        return (
-            self.normalize(torch.tensor(X.values, dtype=torch.float)),
-            self.normalize(torch.tensor(y.values, dtype=torch.float)),
-            y_nanmask,
-        )
 
 
 def calculate_losses(m, x, y, y_nanmasks):
@@ -229,7 +42,7 @@ def calculate_losses(m, x, y, y_nanmasks):
 if __name__ == "__main__":
     torch.manual_seed(42)
 
-    ds = TimelineDS()
+    ds = TimelineDS(BLOCK_SIZE)
 
     # test_subset = Subset(ds, range(0, 1000))
     train_ds, val_ds = random_split(ds, lengths=[0.9, 0.1])
